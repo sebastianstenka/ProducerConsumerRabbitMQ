4d974ed84d7cb9b1707ae60b05a02bc237008c2e
Sebastian Stenka <sebastian.stenka@dgt.eu>
2022-10-28 08:32:29 +0200

    Add project files.

---
+++ Consumer/Receiver.cs
@@ -0,0 +1,33 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory()
+{
+    HostName = "localhost"
+};
+
+using (var connection = factory.CreateConnection())
+{
+    using (var channel = connection.CreateModel())
+    {
+        channel.QueueDeclare("BaseTest", false, false, false, null);
+        channel.CallbackException += (sender, args) =>
+        {
+            Console.WriteLine($"Exception {args.Exception.Message}");
+        };
+
+        var consumer = new EventingBasicConsumer(channel);
+        consumer.Received += (sender, e) =>
+        {
+            var body = e.Body.ToArray();
+            var message = Encoding.UTF8.GetString(body);
+            Console.WriteLine($"Received message: {message}");
+        };
+
+        channel.BasicConsume("BasicTest", true, consumer);
+
+        Console.WriteLine("Press [enter] to exit the Consumer...");
+        Console.ReadLine();
+    }
+}
---
+++ Producer/Sender.cs
@@ -0,0 +1,25 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory()
+{
+    HostName = "localhost",
+};
+
+using(var connection = factory.CreateConnection())
+{
+    using(var channel = connection.CreateModel())
+    {
+        channel.QueueDeclare("BasicTest", false, false, false, null);
+
+        var message = "Message send to RabbitMq queue";
+        var body = Encoding.UTF8.GetBytes(message);
+
+        channel.BasicPublish(string.Empty, "BasicTest", null, body);
+        Console.WriteLine($"SentMessage message {message}");
+
+    }
+}
+
+Console.WriteLine("Press [enter] to exit the Consumer...");
+Console.ReadLine();
